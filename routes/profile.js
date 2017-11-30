9f38b3dc644ab1bd1c6582b29aeba24405c49d28
Mir Ahmed <mir.ahmed39@yahoo.com>
2017-11-29 19:08:17 -0500

    changed multer configuration

--- routes/profile.js
+++ routes/profile.js
@@ -9,9 +9,11 @@ const UserDetails = require('../db').UserDetails;
 const Questionnaire = require('../db').Questionnaire;
 
 // multer configuration
+const storageDest = path.resolve(__dirname, '../public/images');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/images');
+        cb(null, storageDest);
+        //cb(null, './public/images');
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '.jpg');
